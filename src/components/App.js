1163b3f6b934054a8f09819b2d74f4b2cfa681b0
nirbhay kaushik <40763483+Nirbhay91@users.noreply.github.com>
2022-07-19 09:31:44 +0530

    update apps

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,39 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
 		super(props);
+	    this.state = { pHidden : false };
+      //this.updateState = this.updateState.bind(this) 
+      
 	};
+  updateState() { 
+      
+    this.setState({pHidden: !(this.state.pHidden)}) 
+  } 
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+			<button
+        id="click"
+        onClick={
+          this.updateState.bind(this)
+        }
+      >
+        Show paragraph
+      </button>
+      {this.state.pHidden ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null }
     		</div>
     	);
     }
 }
 
 
-export default App;
-
+export default App
